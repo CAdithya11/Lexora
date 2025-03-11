4bc76f14a38ae271d6e74ee51e52b0e20a05fb25
Anjali Sewmini <anjalisewmini5@gmail.com>
2025-03-11 11:31:53 +0530

    Merge changes

--- Frontend/lexora/src/App.jsx
+++ Frontend/lexora/src/App.jsx
@@ -1,30 +1,23 @@
-<<<<<<< HEAD
-import SidebarSub from './component/template/SidebarSub';
 import SearchRoadmapPage from './pages/Functions/Roadmap/SearchRoadmapPage';
-=======
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import RoadmapPage from './pages/Functions/RoadmapPage';
+import RoadmapPage from './pages/Functions/Roadmap/RoadmapPage';
 import TrendingJobsPage from './pages/Functions/Industry Insights/TrendingJobsPage';
-import HomePage from './pages/Home/HomePage';
 import SignIn from './pages/Home/SignIn';
 import SignUpPage from './pages/Home/SignUpPage';
->>>>>>> b8958e14fd471f8d996054334348245ec8750169
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
-      <SearchRoadmapPage/>
-=======
       <BrowserRouter>
         <Routes>
           <Route path="/roadmap" element={<RoadmapPage />} />
           <Route path="/jobTrendings" element={<TrendingJobsPage />} />
           <Route path="/signIn" element={<SignIn />} />
           <Route path="/signUp" element={<SignUpPage />} />
+          <Route path="/srp" element={<SearchRoadmapPage />} />
+
         </Routes>
       </BrowserRouter>
->>>>>>> b8958e14fd471f8d996054334348245ec8750169
     </>
   );
 }
