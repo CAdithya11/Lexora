14d0c0fd077b2e593c6be5ff6001f1ecc182e9c7
Anjali Sewmini <anjalisewmini5@gmail.com>
2025-04-02 17:49:52 +0530

    Roadmap

--- Backend/Lexora/src/main/java/com/NoIdea/Lexora/LexoraApplication.java
+++ Backend/Lexora/src/main/java/com/NoIdea/Lexora/LexoraApplication.java
@@ -1,9 +1,7 @@
 package com.NoIdea.Lexora;
 
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.openqa.selenium.WebDriver;
 
 @SpringBootApplication
 public class LexoraApplication {
--- Backend/Lexora/src/test/java/com/NoIdea/Lexora/LexoraApplicationTests.java
+++ Backend/Lexora/src/test/java/com/NoIdea/Lexora/LexoraApplicationTests.java
@@ -1,7 +1,6 @@
 package com.NoIdea.Lexora;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
