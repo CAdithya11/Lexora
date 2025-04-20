6e0f1b159dfad57b8b6141d0a37df6cad8204def
CAdithya11 <cadithya13@gmail.com>
2025-04-20 13:10:38 +0530

    Github pages

--- Frontend/lexora/src/component/Roadmaps/RoadmapGeminiApi.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Roadmap from './Roadmap';
-
-const RoadmapGeminiApi = ({ data, jobRole, onReset }) => {
-  
-  return <Roadmap JsonRoadmapData={data.html}/>;
-};
-
-export default RoadmapGeminiApi;
---
+++ backend/src/main/java/com/NoIdea/Lexora/LexoraApplication.java
@@ -0,0 +1,13 @@
+package com.NoIdea.Lexora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LexoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LexoraApplication.class, args);
+
+	} 
+}
