61feb3546c7c92aed7f5b91f80c9179aacbb3641
Bishar <mohmedbishar94kd@gmail.com>
2025-05-15 16:07:21 +0530

    Persona matcher backend

--- backend/src/main/java/com/NoIdea/Lexora/model/PersonaMatchingModel/PersonaMatchingModel.java
+++ backend/src/main/java/com/NoIdea/Lexora/model/PersonaMatchingModel/PersonaMatchingModel.java
@@ -1,3 +1,22 @@
-// public class PersonaMatchingController {
-    
-// }
+package com.NoIdea.Lexora.model.PersonaMatchingModel;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class PersonaMatchingModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String No;
+    private String persona;
+    private String matchPrecentage;
+    @Lob
+    private String suggestion;
+}
